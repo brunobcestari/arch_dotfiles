34bc838e8daddd16f71b41209aae59cab9c6ade2
Bruno Cestari <brunobcestari@protonmail.com>
2025-12-28 22:22:47 -0300

    fixing vpn module

--- waybar/scripts/vpn-status.sh
+++ waybar/scripts/vpn-status.sh
@@ -3,10 +3,10 @@
 # Check ProtonVPN status
 ICON_VPN=$'\uf023'
 
-VPN_STATUS=$(nmcli connection show --active | grep -i "vpn\|proton")
+VPN_STATUS=$(nmcli connection show --active | grep -i "protonvpn\|wireguard" | grep -v "killswitch")
 
 if [ -n "$VPN_STATUS" ]; then
-    VPN_NAME=$(echo "$VPN_STATUS" | awk '{print $1}')
+    VPN_NAME=$(echo "$VPN_STATUS" | head -1 | awk '{print $1, $2}')
     echo "{\"text\": \"$ICON_VPN $VPN_NAME\", \"class\": \"connected\", \"tooltip\": \"VPN: Connected to $VPN_NAME\"}"
 else
     echo "{\"text\": \"$ICON_VPN\", \"class\": \"disconnected\", \"tooltip\": \"VPN: Not connected\"}"
